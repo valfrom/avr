7fd3bb3b5dafae7d2d0e1d7b8f5e2949628e9378
Valerii Ivanov <v.ivanov@keitaitoys.com>
2014-07-29 00:21:17 +0300

    New design

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <avr/interrupt.h>
 #include <util/delay.h>
 #include <avr/eeprom.h>
+#include <avr/pgmspace.h>
 
 extern "C" {
     #include "n5110/n5110.h"
@@ -15,6 +16,15 @@ volatile int64_t time=0;
 dht_t dht; 
 int temperature = 0;
 int humidity = 0;
+int speed = 0;
+int avrgSpeed = 0;
+int odo = 0;
+int trip = 0;
+int dst = 0;
+
+int battery = 0;
+
+
 volatile int64_t counter = 0;
 
 #define COUNTER_EEPROM_ADDRESS 0
@@ -96,22 +106,33 @@ void update_dht() {
     }
 }
 
-void printInt(int64_t a, int n=4, int delim=true) {
+void printInt(int64_t a, int n=4, LcdFontSize font=FONT_1X, int delim=true) {
     if(a < 0) {
         a = -a;
     }
     unsigned char *temp = (unsigned char *)itoa(a, n);
     int i;
     for(i=0;i<n;i++) {
-        LcdChr(FONT_1X, temp[i]);
+        LcdChr(font, temp[i]);
     }   
     if(delim) { 
-        LcdChr(FONT_1X, ' ');
+        LcdChr(font, ' ');
     }
 }
 
-void printChar(const char c) {
-    LcdChr(FONT_1X, c);
+void printChar(const char c, LcdFontSize font=FONT_1X) {
+    LcdChr(font, c);
+}
+
+void printStr(const char *str, LcdFontSize font=FONT_1X) {
+    int i;
+    for(i=0;i<100;i++) {
+        char c = pgm_read_byte(str + i);
+        if(c == 0) {
+            break;
+        }
+        printChar(c, font);
+    }
 }
 
 
@@ -149,8 +170,7 @@ void load_data() {
 void write_data() {
     if(oldCounter != counter) {
         oldCounter = counter;
-        eeprom_write_block(&oldCounter, COUNTER_EEPROM_ADDRESS, sizeof(int));
-        
+        eeprom_write_block(&oldCounter, COUNTER_EEPROM_ADDRESS, sizeof(int));        
     }
 }
 
@@ -165,29 +185,91 @@ void setup() {
     LcdContrast(70);
 
     backlit_on();
+
+    speed = 358;
+    avrgSpeed = 235;
+    odo = 40000;
+    trip = 301;
+    dst = 1234;
+
     sei();
 }
 
 
 void loop() {
+
     Lcd_clear();
-    LcdGotoXY(0, 0);
 
-    printInt(temperature, 2, false);
-    printChar('C');
-    printChar(' ');
-    printInt(humidity, 2, false);
-    printChar('%');
-    printChar(' ');
+    battery ++;
+    if(battery > 100) {
+        battery = 0;
+    }
+    int h = 18;
+    int w = 10;
 
-    printInt(counter, 5, false);
-    printChar(' ');
+    Lcd_rect_empty(0, 2, w, h+2, PIXEL_ON);
 
-    printInt(time, 14, false);
+    int gauge = (battery) * h / 100;
+    Lcd_rect(0, h+2, gauge, w, PIXEL_ON);
+
+    Lcd_rect(3, 1, 2, 5, PIXEL_ON);
+
+    int i;
+    for(i=0;i<4;i++) {
+        int y = i * 4 + 5;
+        Lcd_line(0, y, w, y, PIXEL_OFF);
+    }
+
+    
+    if(speed == 0) {
+        LcdGotoXY(0, 0);
+
+        printInt(temperature, 2, FONT_1X, false);
+        printStr(PSTR("C "));
+
+        printInt(humidity, 2, FONT_1X, false);
+        printChar('%');
+    }
+
+    // LcdGotoXY(8, 2);
+
+    // printInt(avrgSpeed / 10, 2, FONT_1X, false);
+
+    LcdGotoXY(7, 1);
+
+    printInt(speed / 10, 2, FONT_2X, false);
+
+    LcdGotoXY(11, 1);
+    printChar('.');
+    printInt(speed % 10, 1, FONT_1X, false);
+
+    LcdGotoXY(11, 0);
+    printStr(PSTR("KMH"));
+
+    LcdGotoXY(0, 3);
+    printStr(PSTR("DST "));
+    printInt(dst / 10, 5, FONT_1X, false);
+    printStr(PSTR("."));
+    printInt(dst % 10, 1, FONT_1X, false);
+    printStr(PSTR(" KM"));
+
+
+    LcdGotoXY(0, 4);
+    printStr(PSTR("TRP "));
+    printInt(trip / 10, 5, FONT_1X, false);
+    printStr(PSTR("."));
+    printInt(trip % 10, 1, FONT_1X, false);
+    printStr(PSTR(" KM"));
+
+    LcdGotoXY(0, 5);
+    printStr(PSTR("ODO "));
+    printInt(odo / 10, 7, FONT_1X, false);
+    printStr(PSTR(" KM"));
+
 
     Lcd_update();
     
-    _delay_ms(1000);
+    _delay_ms(100);
     update_dht();
     write_data();
 }
